326d3853541d5c7c229754879a85c892ca56d478
thunsudamn <140685871+thunsudamn@users.noreply.github.com>
2023-09-12 17:17:27 +0700

    Create index.test.js

---
+++ src/tests/index.test.js
@@ -0,0 +1,20 @@
+src/tests/index.test.js
+
+    const request =  require('supertest')
+    const server =  require('../server')
+    const app = request(server)
+    describe('Get Endpoints', () => {
+        it('Get', async () => {
+            const res =  await app
+            .get('/')
+            .send({
+                name:  'test ran successfully',
+            });
+            expect(res.statusCode).toEqual(200);
+            expect(res.body).toHaveProperty('name');
+            expect(res.body).toHaveProperty('status');
+        })
+    })
+    afterAll(async () => {
+        server.close();
+    });
