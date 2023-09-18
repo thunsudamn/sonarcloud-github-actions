23b510d74acdf8ed5969391d8148969e90a51874
paintparisut <92552125+paintparisut@users.noreply.github.com>
2023-09-18 13:40:14 +0700

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,20 +1,18 @@
-tests/index.test.js
-
-    const request =  require('supertest')
-    const server =  require('../server')
-    const app = request(server)
-    describe('Get Endpoints', () => {
-        it('Get', async () => {
-            const res =  await app
-            .get('/')
-            .send({
-                name:  'test ran successfully',
-            });
-            expect(res.statusCode).toEqual(200);
-            expect(res.body).toHaveProperty('name');
-            expect(res.body).toHaveProperty('status');
-        })
+const request =  require('supertest')
+const server =  require('../server')
+const app = request(server)
+describe('Get Endpoints', () => {
+    it('Get', async () => {
+        const res =  await app
+        .get('/')
+        .send({
+            name:  'test ran successfully',
+        });
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveProperty('name');
+        expect(res.body).toHaveProperty('status');
     })
-    afterAll(async () => {
-        server.close();
-    });
+})
+afterAll(async () => {
+    server.close();
+});
