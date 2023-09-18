804f62500422cf77fb1aa13cb07a00072459113f
thunsudamn <140685871+thunsudamn@users.noreply.github.com>
2023-09-18 11:13:35 +0700

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,7 @@ module.exports = {
   reporters: [
     'default',
     ['jest-sonar', { outputDirectory: 'coverage', outputName: 'test-reporter.xml' }]
-  ]
+  ],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  testResultsProcessor: 'jest-sonar-reporter'
 };
