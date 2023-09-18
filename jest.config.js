f01b5f36d458f2dba2d901f0e071ce6b6b1e2b9e
thunsudamn <140685871+thunsudamn@users.noreply.github.com>
2023-09-18 10:44:22 +0700

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.js$',
   collectCoverage: true,
   coverageReporters: ['lcov', 'text-summary'],
-  reporters: ['default', 'jest-sonar-reporter']
+  reporters: [
+    'default',
+    ['jest-sonar', { outputDirectory: 'coverage', outputName: 'test-reporter.xml' }]
+  ]
 };
